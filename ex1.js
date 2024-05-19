958552976c8791a4319af82cf74441dc68a90915
Emilia-Dumitru <dumitruemilia41@gmail.com>
2024-05-19 15:44:46 +0300

    am inceput tema3

---
+++ ex1.js
@@ -0,0 +1,21 @@
+/*Scrieți o funcție care creează un obiect 
+pentru a păstra informații despre rețeta ta preferată
+*/
+function CreateRecipe(title, portions, ingredients) {
+    const recipe = {
+        title: title,
+        portions: portions,
+        ingredients: ingredients,
+    };
+    return recipe;
+}
+const MyFavRecipe = CreateRecipe("Pizza", 3, ["ham", "cheese","ketchup"])
+console.log(MyFavRecipe);
+
+//Pentru a afisa obiectul fara functie 
+const recipe ={
+    title : "Pizza",
+    portions: 3,
+    ingredients: ["ham", "cheese","ketchup"],
+}
+console.log(recipe);
