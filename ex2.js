9d901187b7086a975e5b81e2674020c260bfeece
Emilia-Dumitru <dumitruemilia41@gmail.com>
2024-05-26 17:01:14 +0300

    am terminat ex2 modificat

--- ex2.js
+++ ex2.js
@@ -1,14 +1,5 @@
 //Scrieți o funcție care returnează un obiect nou fără proprietățile furnizate
-   
-//First method without a function
-const my_object = {
-    a: 1,
-    b: 2,
-}
-delete my_object.b;
-console.log(my_object);
 
-//Method with a function 
 function returnObj(obj, prop) {
     const newObj = {...obj};
     delete newObj[prop];
